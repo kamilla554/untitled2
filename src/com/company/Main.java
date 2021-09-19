a671a848485644c308e4cdd70d08355e27ae4154
User <khayrullina.k.i@edu.mirea.ru>
2021-09-19 16:22:24 +0300

    готово2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 Dog ara = new Dog("boba", 5);
+        System.out.println(ara.toString());
+        System.out.println("human age:"+ara.humanAge(6));
+        DogKennels ker= new DogKennels();
+        ker.test();
+        Shape circle= new Shape(5,6);
+        System.out.println(circle.toString());
     }
 }
